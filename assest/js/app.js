399fafdb02956b158902c88ac6ccaf2f8621f6f7
ravanmaxmud <ravanim@code.edu.az>
2022-09-15 14:25:17 +0400

    changed

--- assest/js/app.js
+++ assest/js/app.js
@@ -1,19 +1,22 @@
 var saat = new Date().getHours()
 if (saat >= 6 && saat <= 12) {
      alert("Sbahun Xeyir")
+     document.body.style.backgroundColor='red'
 } else if (saat >= 13 && saat <= 18) {
 
      alert("Gunortan Xeyir")
+     document.body.style.backgroundColor='green'
 } else {
      alert("Gejen Xeyre");
+     document.body.style.backgroundColor='blue'
 }
-function bas(){
-     if(document.body.style.backgroundColor=="white")
-     {
-          document.body.style.backgroundColor='red'
-     }
-      else
-      {
-          document.body.style.backgroundColor='white'
-      }
-}
+// function bas(){
+//      if(document.body.style.backgroundColor=="white")
+//      {
+//           document.body.style.backgroundColor='red'
+//      }
+//       else
+//       {
+//           document.body.style.backgroundColor='white'
+//       }
+// }
